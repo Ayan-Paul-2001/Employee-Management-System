bce752e5f424db69e5008347eb77def792954fe5
Ayan Paul <ayancse2001@gmail.com>
2025-08-16 01:53:34 +0600

    Admin module + entities added

--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException , BadRequestException} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Admin } from './entities/admin.entity';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/auth/entities/user.entity'; 
+
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(
+    @InjectRepository(Admin)
+    private adminRepo: Repository<Admin>,
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+}
