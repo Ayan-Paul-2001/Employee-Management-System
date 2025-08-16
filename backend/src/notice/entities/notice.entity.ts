190f23a9f1ce6d3d70f17ffa31c16cd0b4387f76
Ayan Paul <ayancse2001@gmail.com>
2025-08-16 19:31:11 +0600

    notice updated

---
+++ backend/src/notice/entities/notice.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+
+
+import { User } from '../../auth/entities/user.entity';
+
+@Entity()
+export class Notice {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'adminId' })
+  admin: User;
+
+  @Column()
+  adminId: number;
+
+  @Column()
+  adminName: string;
+
+  @Column()
+  type: string;
+
+  @Column({ type: 'text' })
+  message: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
